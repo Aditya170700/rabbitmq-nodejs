01d86cf8a8b8d1ff641cfbc4609a789b128dc5e3
Aditya170700 <adityaric72@gmail.com>
2024-06-30 15:43:47 +0700

    Consumer

---
+++ consumer.js
@@ -0,0 +1,15 @@
+import {connect} from "amqplib";
+
+const connection = await connect("amqp://guest:guest@localhost:5672/");
+const channel = await connection.createChannel();
+
+// TODO Consumer
+await channel.consume("sms", function (message) {
+    console.log(message.fields.routingKey);
+    console.log(message.content.toString());
+}, {
+    noAck: true
+});
+
+// await channel.close();
+// await connection.close();
